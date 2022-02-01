e066a4fc62975155b4fb5ea586b784840429c773
Sidarth Venkatesan <64766301+Sidarth-V@users.noreply.github.com>
2022-02-01 09:37:40 +0530

    init

---
+++ solve_me.py
@@ -0,0 +1,124 @@
+class TasksCommand:
+    TASKS_FILE = "tasks.txt"
+    COMPLETED_TASKS_FILE = "completed.txt"
+
+    current_items = {}
+    completed_items = []
+
+    def read_current(self):
+        try:
+            file = open(self.TASKS_FILE, "r")
+            for line in file.readlines():
+                item = line[:-1].split(" ")
+                self.current_items[int(item[0])] = " ".join(item[1:])
+            file.close()
+        except Exception:
+            pass
+
+    def read_completed(self):
+        try:
+            file = open(self.COMPLETED_TASKS_FILE, "r")
+            self.completed_items = file.readlines()
+            file.close()
+        except Exception:
+            pass
+
+    def write_current(self):
+        with open(self.TASKS_FILE, "w+") as f:
+            f.truncate(0)
+            for key in sorted(self.current_items.keys()):
+                f.write(f"{key} {self.current_items[key]}\n")
+
+    def write_completed(self):
+        with open(self.COMPLETED_TASKS_FILE, "w+") as f:
+            f.truncate(0)
+            for item in self.completed_items:
+                f.write(f"{item}\n")
+
+    def run(self, command, args):
+        self.read_current()
+        self.read_completed()
+        if command == "add":
+            self.add(args)
+        elif command == "done":
+            self.done(args)
+        elif command == "delete":
+            self.delete(args)
+        elif command == "ls":
+            self.ls()
+        elif command == "report":
+            self.report()
+        elif command == "help":
+            self.help()
+
+    def help(self):
+        print(
+            """Usage :-
+$ python tasks.py add 2 hello world # Add a new item with priority 2 and text "hello world" to the list
+$ python tasks.py ls # Show incomplete priority list items sorted by priority in ascending order
+$ python tasks.py del PRIORITY_NUMBER # Delete the incomplete item with the given priority number
+$ python tasks.py done PRIORITY_NUMBER # Mark the incomplete item with the given PRIORITY_NUMBER as complete
+$ python tasks.py help # Show usage
+$ python tasks.py report # Statistics"""
+        )
+
+    def add(self, args):
+        try:
+            self.read_current()
+            if int(args[0]) in self.current_items.keys():
+                call = []
+                old_item = self.current_items[int(args[0])]
+                new_priority = int(args[0]) + 1
+                call.append(new_priority)
+                call.append(old_item)
+                self.add(call)
+                self.current_items[int(args[0])] = args[1]
+            else:
+                self.current_items[int(args[0])] = args[1]
+            self.write_current()
+            print(f"Added task: \"{args[1]}\" with priority {args[0]}")
+        except Exception:
+            print("here")
+
+    def done(self, args):
+        try:
+            self.read_current()
+            self.read_completed()
+            item = self.current_items[int(args[0])]
+            self.completed_items.append(item)
+            del self.current_items[int(args[0])]
+            self.write_current()
+            self.write_completed()
+            print("Marked item as done.")
+        except Exception:
+            print(f"Error: no incomplete item with priority {args[0]} exists.")
+
+    def delete(self, args):
+        try:
+            self.read_current()
+            del self.current_items[int(args[0])]
+            self.write_current()
+            print(f"Deleted item with priority {args[0]}")
+        except Exception:
+            print(f"Error: item with priority {args[0]} does not exist. Nothing deleted.")
+
+    def ls(self):
+        self.read_current()
+        i = 1
+        for priority, task in self.current_items.items():
+            print(f"{i}. {task} [{priority}]")
+            i = i + 1
+
+    def report(self):
+        self.read_current()
+        self.read_completed()
+        print(f"Pending : {len(self.current_items)}")
+        i = 1
+        for priority, task in self.current_items.items():
+            print(f"{i}. {task} [{priority}]")
+            i = i + 1
+        print(f"\nCompleted : {len(self.completed_items)}")
+        i = 1
+        for item in self.completed_items:
+            print(f"{i}. {item}")
+            i = i + 1
